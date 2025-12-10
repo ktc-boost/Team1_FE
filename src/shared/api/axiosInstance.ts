5683278420c6d8ae9a9dd10526d569859bbb1edc
hyemin <hyemin6647@naver.com>
2025-12-10 17:21:13 +0900

    ♻️ (#337) 인터셉터에서 전역 에러만 처리

--- src/shared/api/axiosInstance.ts
+++ src/shared/api/axiosInstance.ts
@@ -1,11 +1,10 @@
 import { useAuthStore } from '@/features/auth/store/useAuthStore';
 import axios from 'axios';
-import { handleGeneralApiError } from '@/shared/api/errorHandler';
-import { handleUnauthorizedRequest } from '@/shared/api/authIntercepter';
-// TODO: 인증 불필요 API에도 토큰이 붙음 → 보안상 불필요. 나중에 apiPublic 같은 인스턴스 분리 고려
+import { handleUnauthorizedRequest } from '@/shared/api/interceptors/handleUnauthorizedRequest';
+import { ApiError } from '@/shared/api/error/ApiError';
 export const api = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  withCredentials: true, //쿠키를 포함한 요청을 보냅니다.
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
@@ -31,13 +30,16 @@ api.interceptors.response.use(
   (res) => res,
   async (error) => {
     const originalRequest = error.config;
-
-    if (error.response?.status === 401) {
+    if (!error.response) {
+      throw new ApiError('NETWORK_ERROR', 0);
+    }
+    if (error.response.status === 401) {
       return handleUnauthorizedRequest(originalRequest);
     }
-    //인증 외 일반 API 에러
-    handleGeneralApiError(error);
-    return Promise.reject(error);
+    if (error.respense.status === 500) {
+      throw new ApiError('SERVER_ERROR', 500);
+    }
+    throw error;
   },
 );
 
