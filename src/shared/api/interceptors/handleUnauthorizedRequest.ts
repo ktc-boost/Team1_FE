96c4cbae22fc0ac405b28a0fd1b3d3f84adf8e78
hyemin <hyemin6647@naver.com>
2025-12-22 15:22:45 +0900

    🐛 토큰 만료 401에러 ApiError로 통일

--- src/shared/api/interceptors/handleUnauthorizedRequest.ts
+++ src/shared/api/interceptors/handleUnauthorizedRequest.ts
@@ -1,6 +1,7 @@
 import axios, { type AxiosRequestConfig } from 'axios';
 import { useAuthStore } from '@/features/auth/store/useAuthStore';
 import { apiPublic } from '@/shared/api/axiosInstance';
+import { ApiError } from '@/shared/api/error/ApiError';
 interface AxiosRequestConfigWithRetry extends AxiosRequestConfig {
   _retry?: boolean;
 }
@@ -15,10 +16,10 @@ export const handleUnauthorizedRequest = async (originalRequest: AxiosRequestCon
     if (!originalRequest.headers) originalRequest.headers = {};
     originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
 
-    return axios(originalRequest); 
+    return axios(originalRequest);
   } catch {
     const clearAuth = useAuthStore.getState().clearAuth;
     clearAuth();
-    return Promise.reject('UNAUTHORIZED');
+    return Promise.reject(new ApiError('UNAUTHORIZED', 401));
   }
 };
