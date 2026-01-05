98fb609c24ba31247a7d50cd73c480107e06526f
hyemin <hyemin6647@naver.com>
2026-01-05 17:41:13 +0900

    ♻️ (#350) 알림 설정 페이지 QR 섹션  컴포넌트 분리

--- src/pages/AlarmSetupPage.tsx
+++ src/pages/AlarmSetupPage.tsx
@@ -1,13 +1,12 @@
 import { motion } from 'framer-motion';
-import { QRCodeSVG } from 'qrcode.react';
-import { AlarmClock, ArrowDown, BellRing, SquareCheck } from 'lucide-react';
+import { AlarmClock, BellRing, SquareCheck } from 'lucide-react';
 import BooAlarmClick from '@/shared/assets/images/boost/boo-with-alarm.png';
 import AlarmBell from '@/shared/assets/images/boost/alarm-bell.png';
 import CircleBox from '@/shared/components/ui/CircleBox';
 import { floatVariant, shakeVariant } from '@/shared/utils/animations/motionVariants';
 import { Button } from '@/shared/components/shadcn/button';
-import InlineLoader from '@/shared/components/ui/loading/InlineLoader';
 import { useAlarmSetup } from '@/features/webpush/hooks/useAlarmSetup';
+import QRCodeSection from './../features/webpush/components/QRCodeSection';
 
 const AlarmSetupPage = () => {
   const { qrData, isPending, timeLeft, handleSkip } = useAlarmSetup();
@@ -46,35 +45,7 @@ const AlarmSetupPage = () => {
         aria-label="QR code setup instructions"
         className="flex flex-col items-center justify-between gap-6 w-[50%] pt-10 box-border"
       >
-        {/* 상단 텍스트 */}
-        <div aria-label="text" className="flex flex-col items-center gap-3">
-          <div className="text-4xl font-semibold text-center">
-            <span className="text-boost-blue-light">알림</span>을 허용해보세요!
-          </div>
-          <div className="text-gray-600 subtitle1-bold text-center">
-            모바일로 하단의 QR 코드를 스캔해주세요!
-          </div>
-          <ArrowDown className="text-boost-blue-light" />
-        </div>
-
-        {/* QR 코드 */}
-        <div className="p-4 shadow-md rounded-md bg-white mt-[-20px]">
-          {isPending ? (
-            <p className="text-gray-500 text-sm text-center w-40 h-40 flex items-center justify-center">
-              <InlineLoader size={6} text="QR 코드 생성 중.." />
-            </p>
-          ) : qrData ? (
-            <QRCodeSVG value={qrData} className="w-40 h-40" />
-          ) : (
-            <p className="text-gray-500 text-sm text-center w-40 h-40 flex items-center justify-center">
-              <InlineLoader size={6} text="QR 데이터 불러오는 중.." />
-            </p>
-          )}
-        </div>
-
-        {/* 남은 시간 */}
-        <p className="text-gray-700 font-semibold text-sm">QR 갱신까지 남은 시간: {timeLeft}</p>
-
+        <QRCodeSection isPending={isPending} qrData={qrData} timeLeft={timeLeft} />
         <Button
           variant="link"
           onClick={() => handleSkip()}
