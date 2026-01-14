30bd42f3f8d034aa484252c22a7070d14f92fd94
daaoooy <y020d226y@daum.net>
2026-01-15 01:16:08 +0900

    ♻️ (#342) 할 일 생성 모달 리팩터링
    - TaskFormField 공통 컴포넌트 적용
    - FormProvider를 적용하여 하위 컴포넌트에 폼 상태 주입

--- src/features/task/components/TaskModal/TaskCreateModalContent.tsx
+++ src/features/task/components/TaskModal/TaskCreateModalContent.tsx
@@ -1,37 +1,16 @@
-import {
-  Calendar,
-  User,
-  Siren,
-  FileText,
-  Loader,
-  Check,
-  NotebookPen,
-  Folder,
-  TagIcon,
-} from 'lucide-react';
-import toast from 'react-hot-toast';
 import { useState, useEffect } from 'react';
+import toast from 'react-hot-toast';
+import { FormProvider } from 'react-hook-form';
 import { Button } from '@/shared/components/shadcn/button';
 import { DialogFooter } from '@/shared/components/shadcn/dialog';
-import { Input } from '@/shared/components/shadcn/input';
-import { Textarea } from '@/shared/components/shadcn/textarea';
-import { FormField } from '@/shared/components/ui/form/FormField';
 import { useModal } from '@/shared/hooks/useModal';
-import { toggleArrayItem } from '@/shared/utils/arrayUtils';
-import { cn } from '@/shared/lib/utils';
-import StatusButtons from '@/shared/components/ui/form/StatusButtons';
-import UrgentToggle from '@/shared/components/ui/form/UrgentToggle';
-import AssigneeDropdown from '@/shared/components/ui/form/AssigneeDropdown';
-import ProjectSelect from '@/shared/components/ui/form/ProjectSelect';
-import TagManager from '@/features/tag/components/TagInput/TagManager';
-import { useCreateTaskForm } from '@/features/task/hooks/form/useCreateTaskForm';
-import { statusList } from '@/features/board/types/boardTypes';
 import { useProjectsQuery } from '@/features/project/hooks/useProjectsQuery';
 import { useCreateTaskMutation } from '@/features/task/hooks/mutation/useCreateTaskMutation';
 import { useProjectMembersQuery } from '@/features/project/hooks/useProjectMembersQuery';
-import type { Tag } from '@/features/tag/types/tagTypes';
+import { useCreateTaskForm } from '@/features/task/hooks/form/useCreateTaskForm';
+import TaskFormField from '@/features/task/components/TaskModal/TaskFormField';
 import { getTagIds } from '@/features/tag/utils/tagUtils';
-import DueDatePicker from '@/shared/components/ui/form/DueDatePicker';
+import type { Tag } from '@/features/tag/types/tagTypes';
 
 interface TaskCreateModalContentProps {
   isMyTask: boolean;
@@ -42,7 +21,6 @@ const TaskCreateModalContent = ({
   isMyTask,
   projectId: propProjectId,
 }: TaskCreateModalContentProps) => {
-  const inputClasses = 'hover:bg-gray-200 focus:ring-transparent h-11 label2-regular';
   const { resetModal } = useModal();
   const { data: projects } = useProjectsQuery();
   const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
@@ -50,6 +28,7 @@ const TaskCreateModalContent = ({
   const { form, handleConfirm, isLoading } = useCreateTaskForm(
     propProjectId ?? '',
     async (taskData) => {
+      const selectedProjectId = form.getValues('projectId') || propProjectId;
       if (!selectedProjectId) {
         toast.error('프로젝트를 선택해주세요.');
         return;
@@ -58,7 +37,7 @@ const TaskCreateModalContent = ({
       try {
         const payload = { ...taskData, tags: getTagIds(selectedTags) };
         await createTask(payload);
-        toast.success('할 일이 성공적으로 생성되었습니다!');
+        toast.success('할 일이 생성되었습니다!');
         resetModal();
       } catch (err) {
         console.log(err);
@@ -67,121 +46,26 @@ const TaskCreateModalContent = ({
     },
   );
 
-  const {
-    register,
-    setValue,
-    watch,
-    formState: { errors },
-  } = form;
-  const assignees = watch('assignees');
-  const status = watch('status');
-  const urgent = watch('urgent') || false;
-  const selectedProjectId = watch('projectId') || propProjectId;
-
+  const selectedProjectId = form.watch('projectId') || propProjectId;
   const { mutate: createTask } = useCreateTaskMutation(selectedProjectId ?? '');
   const { data: projectMembers } = useProjectMembersQuery(selectedProjectId);
 
   useEffect(() => {
-    if (isMyTask && !watch('projectId') && projects?.length) {
-      setValue('projectId', projects[0].id);
+    if (isMyTask && !form.watch('projectId') && projects?.length) {
+      form.setValue('projectId', projects[0].id);
     }
-  }, [isMyTask, projects, setValue, watch]);
+  }, [isMyTask, projects, form]);
 
   return (
-    <>
-      <div className="flex flex-col gap-8 py-4 max-h-[400px] overflow-y-auto px-1">
-        {/* 프로젝트 선택 */}
-        {isMyTask && (
-          <FormField icon={Folder} required label="프로젝트" error={errors.projectId?.message}>
-            <ProjectSelect
-              selectedProjectId={watch('projectId')}
-              projects={projects || []}
-              onProjectSelect={(id) => setValue('projectId', id, { shouldValidate: true })}
-            />
-          </FormField>
-        )}
-
-        {/* 제목 */}
-        <FormField icon={FileText} required label="제목" error={errors.title?.message}>
-          <Input
-            {...register('title')}
-            placeholder="예: 로그인 페이지 UI 구현하기"
-            className={inputClasses}
-          />
-        </FormField>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          {/* 진행 상태 */}
-          <FormField icon={Loader} required label="진행 상태" error={errors.status?.message}>
-            <StatusButtons
-              statusList={statusList}
-              selectedStatus={status}
-              setStatus={(s) => setValue('status', s)}
-            />
-          </FormField>
-
-          {/* 긴급 여부 */}
-          <FormField icon={Siren} label="긴급 여부">
-            <UrgentToggle urgent={urgent} setUrgent={(v) => setValue('urgent', v)} />
-          </FormField>
-        </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          {/* 담당자 */}
-          <FormField icon={User} required label="담당자" error={errors.assignees?.message}>
-            <AssigneeDropdown
-              disabled={!projectMembers?.length}
-              assignees={assignees}
-              toggleAssignee={(name) =>
-                setValue('assignees', toggleArrayItem(assignees, name), { shouldValidate: true })
-              }
-              members={projectMembers ?? []}
-            />
-          </FormField>
-
-          {/* 마감일 */}
-          <FormField icon={Calendar} required label="마감일" error={errors.dueDate?.message}>
-            <DueDatePicker
-              value={watch('dueDate')}
-              onChange={(date) => setValue('dueDate', date, { shouldValidate: true })}
-            />
-          </FormField>
-        </div>
-
-        {/* 태그 */}
-        <FormField icon={TagIcon} label="태그" error={errors.tags?.message}>
-          <TagManager
-            projectId={selectedProjectId ?? ''}
-            selectedTags={selectedTags}
-            onChangeTags={setSelectedTags}
-          />
-        </FormField>
-
-        {/* 필요한 검토 수 */}
-        <FormField
-          icon={Check}
-          label="필요한 검토 수"
-          error={errors.requiredReviewerCount?.message}
-        >
-          <Input
-            type="number"
-            min={0}
-            {...register('requiredReviewerCount', { valueAsNumber: true })}
-            className={inputClasses}
-          />
-        </FormField>
-
-        {/* 상세 설명 */}
-        <FormField icon={NotebookPen} label="상세 설명" error={errors.description?.message}>
-          <Textarea
-            {...register('description')}
-            placeholder="할 일에 대한 상세 설명을 입력하세요"
-            className={cn('!h-25', inputClasses)}
-          />
-        </FormField>
-      </div>
-
-      {/* 하단 버튼 영역 (취소, 생성) */}
+    <FormProvider {...form}>
+      <TaskFormField
+        selectedTags={selectedTags}
+        setSelectedTags={setSelectedTags}
+        isMyTask={isMyTask}
+        projects={projects || []}
+        projectMembers={projectMembers ?? []}
+        projectId={propProjectId}
+      />
       <DialogFooter className="gap-2 pt-4 border-t border-gray-300">
         <Button
           onClick={resetModal}
@@ -199,7 +83,7 @@ const TaskCreateModalContent = ({
           {isLoading ? '생성 중...' : '할 일 생성'}
         </Button>
       </DialogFooter>
-    </>
+    </FormProvider>
   );
 };
 
