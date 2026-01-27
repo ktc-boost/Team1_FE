4c159aadc75d5021b4aaa4ecf9c5a6be9240bd92
hyemin <hyemin6647@naver.com>
2026-01-27 17:11:04 +0900

    ⚡️ (#355) 할일상세, 메모, PDF업로더 lazy 로딩 적용

--- src/features/board/components/BoardSection.tsx
+++ src/features/board/components/BoardSection.tsx
@@ -1,11 +1,12 @@
 import { useOutletContext } from 'react-router-dom';
-import StatusBoard from '@/features/board/components/StatusBoard/StatusBoard';
-import MemberBoard from '@/features/board/components/MemberBoard/MemberBoard';
 import ProjectFilterTab from '@/features/project/components/ProjectPageComponents/ProjectFilterTab';
 import MyTaskFilterTab from '@/features/my-task/components/MyTaskPageComponents/MyTaskFilterTab';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, lazy, Suspense } from 'react';
 import { Separator } from '@/shared/components/shadcn/separator';
 import { useSortStore } from '@/features/board/store/useSortStore';
+
+const StatusBoard = lazy(() => import('@/features/board/components/StatusBoard/StatusBoard'));
+const MemberBoard = lazy(() => import('@/features/board/components/MemberBoard/MemberBoard'));
 
 interface BoardSectionProps {
   type: 'project' | 'myTask';
@@ -48,7 +49,10 @@ const BoardSection = ({ type, boardTab: initialTab }: BoardSectionProps) => {
 
       <Separator className="bg-gray-300" />
 
-      <div className="flex-1 min-h-0 overflow-x-auto">{renderBoard()}</div>
+      <div className="flex-1 min-h-0 overflow-x-auto">
+        {/* 📍 suspense에 스켈레톤 ui 필요*/}
+        <Suspense fallback={<div className="p-3">보드 불러오는 중…</div>}>{renderBoard()}</Suspense>
+      </div>
     </div>
   );
 };
--- src/features/task-detail/components/FileSection/FileSection.tsx
+++ src/features/task-detail/components/FileSection/FileSection.tsx
@@ -1,10 +1,11 @@
-import { Link, Upload } from 'lucide-react';
+import { Link } from 'lucide-react';
 import FileItem from '@/features/task-detail/components/FileSection/FileItem';
 import { useTaskFilesQuery } from '@/features/task-detail/hooks/useTaskFilesQuery';
 import type { ServerFileType } from '@/features/task-detail/types/fileApiTypes';
 import ContentItem from '@/shared/components/ui/ContentItem';
-import { useFileUploader } from '@/features/task-detail/hooks/useFileUploader';
 import { useDeleteFileMutation } from '@/features/task-detail/hooks/useDeleteFileMutation';
+import { Suspense } from 'react';
+import { FileUploadAction } from '@/features/task-detail/components/FileSection/FileUploadAction';
 
 interface FileSectionProps {
   onOpenPdf: (url: string, fileName: string, id: string) => void;
@@ -15,7 +16,6 @@ interface FileSectionProps {
 const FileSection = ({ onOpenPdf, taskId, files: serverFiles }: FileSectionProps) => {
   const { data: uiFiles } = useTaskFilesQuery(serverFiles, taskId);
   const { mutate: deleteFile } = useDeleteFileMutation(taskId);
-  const { getRootProps, getInputProps } = useFileUploader(taskId);
 
   return (
     <div className="w-full h-full pt-6 p-3 pb-4 border-t-2 border-gray-300 flex flex-col">
@@ -23,10 +23,9 @@ const FileSection = ({ onOpenPdf, taskId, files: serverFiles }: FileSectionProps
         icon={Link}
         title="첨부파일"
         action={
-          <div {...getRootProps()} className="cursor-pointer">
-            <input {...getInputProps()} />
-            <Upload className="w-5 h-5 text-gray-900" />
-          </div>
+          <Suspense fallback={<div className="w-5 h-5" />}>
+            <FileUploadAction taskId={taskId} />
+          </Suspense>
         }
       />
       <div className="flex flex-col gap-2 overflow-y-auto pt-3 mb-5">
