be4c384dbe13be478779b7fb530e6d65281da304
hyemin <hyemin6647@naver.com>
2026-01-31 10:30:55 +0900

    💄 (#354) 아바타 저장 버튼 너비 수정

--- src/features/avatar-picker/components/AvatarSaveBtn.tsx
+++ src/features/avatar-picker/components/AvatarSaveBtn.tsx
@@ -1,5 +1,4 @@
 import { Button } from '@/shared/components/shadcn/button';
-import { Check } from 'lucide-react';
 interface AvatarSaveBtnProps {
   handleSave: () => void;
 }
@@ -8,14 +7,10 @@ const AvatarSaveBtn = ({ handleSave }: AvatarSaveBtnProps) => {
     <div className="pt-12">
       <Button
         onClick={handleSave}
-        className="w-0 sm:w-80 bg-boost-blue hover:bg-boost-blue-hover text-white font-semibold text-lg h-14 rounded-2xl shadow-lg hover:shadow-xl transform hover:-translate-y-1 transition-all duration-300 relative overflow-hidden group"
+        className="w-50 sm:w-80 bg-boost-blue hover:bg-boost-blue-hover cursor-pointer text-white font-semibold text-md h-14 rounded-2xl shadow-lg hover:shadow-xl transform hover:-translate-y-1 transition-all duration-300 relative overflow-hidden group"
         size="lg"
       >
-        <div className="absolute inset-0 bg-gradient-to-r from-transparent via-white to-transparent opacity-0 group-hover:opacity-20 transform -translate-x-full group-hover:translate-x-full transition-transform duration-700" />
-        <span className="relative z-10 flex items-center gap-2">
-          <Check size={20} />
-          완료
-        </span>
+        완료
       </Button>
     </div>
   );
