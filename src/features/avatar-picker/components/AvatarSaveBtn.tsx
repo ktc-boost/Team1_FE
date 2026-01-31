81a36ba7ac77c80fd58c26bb0f2f67ebc8637455
hyemin <hyemin6647@naver.com>
2026-02-01 08:42:01 +0900

    ♻️ (#354) 버튼 스타일을 defaultBoost variant로 통일

--- src/features/avatar-picker/components/AvatarSaveBtn.tsx
+++ src/features/avatar-picker/components/AvatarSaveBtn.tsx
@@ -7,7 +7,8 @@ const AvatarSaveBtn = ({ handleSave }: AvatarSaveBtnProps) => {
     <div className="pt-12">
       <Button
         onClick={handleSave}
-        className="w-50 sm:w-80 bg-boost-blue hover:bg-boost-blue-hover cursor-pointer text-white font-semibold text-md h-14 rounded-2xl shadow-lg hover:shadow-xl transform hover:-translate-y-1 transition-all duration-300 relative overflow-hidden group"
+        variant={'defaultBoost'}
+        className="w-50 sm:w-80 font-semibold text-md h-14 rounded-2xl hover:shadow-lg hover:-translate-y-1 transition"
         size="lg"
       >
         완료
