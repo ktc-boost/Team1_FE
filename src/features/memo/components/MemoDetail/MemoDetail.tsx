a711a959ab96198c41acd57ba875f0ab594a3fcd
daaoooy <y020d226y@daum.net>
2026-02-01 23:19:16 +0900

    ✨ (#364, #377) 메모 상세 반응형 도입 및 코드 리팩터링
    - 반응형 도입
    - 공통 컴포넌트 MetaItem 도입
    - 코드 가독성 및 유지보수성 개선

--- src/features/memo/components/MemoDetail/MemoDetail.tsx
+++ src/features/memo/components/MemoDetail/MemoDetail.tsx
@@ -11,21 +11,16 @@ const MemoDetail = () => {
   if (!projectId) return <div className="p-4">프로젝트 ID를 찾을 수 없습니다.</div>;
   if (!memoId) return <div className="p-4">메모 ID를 찾을 수 없습니다.</div>;
 
-  if (isLoading) {
-    return <FullPageLoader />;
-  }
-
-  if (isError || !memo) {
-    return <div className="p-4">메모를 불러오는 데 실패했습니다.</div>;
-  }
+  if (isLoading) return <FullPageLoader />;
+  if (isError || !memo) return <div className="p-4">메모를 불러오는 데 실패했습니다.</div>;
 
   return (
-    <div className="flex flex-col bg-gray-200 border-t border-t-gray-300 h-full overflow-hidden">
-      <div className="flex flex-col h-full bg-gray-100 p-4 space-y-4 overflow-hidden rounded-xl shadow-[0_0_6px_rgba(0,0,0,0.08)] m-3">
+    <article className="flex flex-col h-full overflow-hidden bg-gray-200 border-t border-t-gray-300">
+      <div className="flex flex-col h-full p-4 m-3 space-y-4 overflow-hidden bg-gray-100 rounded-xl shadow-[0_0_6px_rgba(0,0,0,0.08)]">
         <MemoDetailHeader memo={memo} projectId={projectId} />
         <MemoDetailContent content={memo.content} />
       </div>
-    </div>
+    </article>
   );
 };
 
