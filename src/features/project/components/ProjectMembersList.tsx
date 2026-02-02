7660928e10f749690a637c766071c2842b9bd20d
daaoooy <y020d226y@daum.net>
2026-02-02 21:08:02 +0900

    ✨ (#365) Project Member List 반응형 도입
    - 모바일, 태블릿, 데스크탑 기준으로 반응형 도입
    - 컴포넌트명 수정 ProjectMembers -> ProjectMembersList

--- src/features/project/components/ProjectMembersList.tsx
+++ src/features/project/components/ProjectMembersList.tsx
@@ -1,14 +1,15 @@
 import ProjectMemberItem from '@/features/project/components/ProjectMemberItem';
 import type { MemberWithBoosting } from '@/features/project/types/projectTypes';
 
-interface ProjectMembersProps {
+interface ProjectMembersListProps {
   members: MemberWithBoosting[];
 }
 
-const ProjectMembers = ({ members }: ProjectMembersProps) => {
+const ProjectMembersList = ({ members }: ProjectMembersListProps) => {
   return (
     <div className="flex-1.5 overflow-y-auto overflow-x-hidden border-r border-gray-300">
-      <div className="w-[40vh] space-y-2">
+      <div className="space-y-2 min-w-[220px] md:min-w-[260px] lg:min-w-[320px]">
+        <p className="md:hidden label1-bold pl-2">프로젝트 멤버</p>
         {members.map((member) => (
           <ProjectMemberItem key={member.id} member={member} />
         ))}
@@ -17,4 +18,4 @@ const ProjectMembers = ({ members }: ProjectMembersProps) => {
   );
 };
 
-export default ProjectMembers;
+export default ProjectMembersList;
