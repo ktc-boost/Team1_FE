42a100479e49b392870322302a6076e8abda9ca8
daaoooy <y020d226y@daum.net>
2026-02-02 20:17:13 +0900

    💄 Shadcn Button 컴포넌트에 outline 버튼 디자인 2가지 추가

--- src/shared/components/shadcn/button.tsx
+++ src/shared/components/shadcn/button.tsx
@@ -18,6 +18,10 @@ const buttonVariants = cva(
           'bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
         outline:
           'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
+        outlineBoost:
+          'border text-boost-blue border border-boost-blue hover:text-white hover:bg-boost-blue bg-background shadow-xs',
+        outlineSecondaryBoost:
+          'border text-boost-orange border border-boost-orange hover:text-white hover:bg-boost-orange bg-background shadow-xs',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
         link: 'text-primary underline-offset-4 hover:underline',
