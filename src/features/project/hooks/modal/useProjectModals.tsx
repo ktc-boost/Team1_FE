d22b3472558c4e4c00a582d954d94525adb0cc86
daaoooy <y020d226y@daum.net>
2026-02-03 00:06:17 +0900

    ♻️ (#389, #391) 액션 함수 추가 및 리팩터링 사항 적용
    - 프로젝트 떠나기, 추방, 삭제 show 액션 함수 추가
    - Create Mutation에서 navigate 분리함에 따라 액션 함수 내에 작성
    - useJoinProject 커스텀 훅 적용

--- src/features/project/hooks/modal/useProjectModals.tsx
+++ src/features/project/hooks/modal/useProjectModals.tsx
@@ -1,14 +1,21 @@
-import { useModal } from '@/shared/hooks/useModal';
 import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '@/app/routes/Router';
+import { useModal } from '@/shared/hooks/useModal';
+import type { MemberWithBoosting } from '@/features/project/types/projectTypes';
+import { useCreateProjectMutation } from '@/features/project/hooks/mutation/useCreateProjectMutation';
+import { useJoinProject } from '@/features/project/hooks/domain/useJoinProject';
 import ProjectCreateModalContent from '@/features/project/components/ProjectCreateModal/ProjectCreateModalContent';
 import ProjectJoinCodeInputModalContent from '@/features/project/components/ProjectJoinModal/ProjectJoinCodeInputModalContent';
-import { useCreateProjectMutation } from '@/features/project/hooks/mutation/useCreateProjectMutation';
-import { useJoinProjectMutation } from '@/features/project/hooks/mutation/useProjectJoinMutation';
+import ProjectDeleteModalContent from '@/features/project/components/ProjectDeleteModal/ProjectDeleteModalContent';
+import ProjectLeaveModalContent from '@/features/project/components/ProjectLeaveModal/ProjectLeaveModalContent';
+import ProjectKickMemberModalContent from '@/features/project/components/ProjectKickMemberModal/ProjectKickMemberModalContent';
 
 export const useProjectModals = () => {
   const { showCustom } = useModal();
-  const createProjectMutation = useCreateProjectMutation();
-  const { mutateAsync: joinProjectMutation } = useJoinProjectMutation();
+  const { joinProject } = useJoinProject();
+  const { mutateAsync: createProjectMutation } = useCreateProjectMutation();
+  const navigate = useNavigate();
 
   const showCreateProjectModal = () => {
     showCustom({
@@ -18,10 +25,10 @@ export const useProjectModals = () => {
         <ProjectCreateModalContent
           onConfirm={async (projectName) => {
             try {
-              await createProjectMutation.mutateAsync(projectName);
+              const createdProject = await createProjectMutation(projectName);
               toast.success('프로젝트가 성공적으로 생성되었습니다!');
-            } catch (error) {
-              console.error('프로젝트 생성 실패:', error);
+              navigate(ROUTES.PROJECT_BOARD(createdProject.id));
+            } catch {
               toast.error('프로젝트 생성에 실패했어요.');
             }
           }}
@@ -37,14 +44,48 @@ export const useProjectModals = () => {
       description: '프로젝트 참여 코드를 입력하면, 프로젝트에 참여할 수 있어요.',
       content: (
         <ProjectJoinCodeInputModalContent
-          onConfirm={async (joinCode) => {
-            await joinProjectMutation(joinCode);
-          }}
+          onConfirm={joinProject}
           onCreateClick={showCreateProjectModal}
         />
       ),
     });
   };
 
-  return { showCreateProjectModal, showJoinProjectModal };
+  const showDeleteProjectModal = () => {
+    showCustom({
+      title: '프로젝트 삭제',
+      titleAlign: 'center',
+      description: '정말로 프로젝트를 삭제하시나요? 🥹',
+      size: 'sm',
+      content: <ProjectDeleteModalContent />,
+    });
+  };
+
+  const showLeaveProjectModal = () => {
+    showCustom({
+      title: '프로젝트 떠나기',
+      description: '정말로 프로젝트를 떠나시나요? 🥹',
+      titleAlign: 'center',
+      size: 'sm',
+      content: <ProjectLeaveModalContent />,
+    });
+  };
+
+  const showKickProjectMemberModal = (projectId: string, member: MemberWithBoosting) => {
+    showCustom({
+      title: '멤버 추방',
+      description: `${member.name}님을 프로젝트에서 추방하시겠어요?`,
+      titleAlign: 'center',
+      size: 'sm',
+      content: <ProjectKickMemberModalContent projectId={projectId} member={member} />,
+    });
+  };
+
+  return {
+    showCreateProjectModal,
+    showJoinProjectModal,
+    showDeleteProjectModal,
+    showLeaveProjectModal,
+    showKickProjectMemberModal,
+  };
 };
