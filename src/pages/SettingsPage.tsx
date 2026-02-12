1a8543abd1ce0b6799bfae5e4d561d52c54ffd00
hyemin <hyemin6647@naver.com>
2026-02-13 00:37:35 +0900

    ✨(#403) 설정 페이지에 BackButton 추가

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -7,6 +7,7 @@ import { Separator } from '@/shared/components/shadcn/separator';
 import AlarmSettingCard from '@/features/settings/components/AlarmSettingCard';
 import { UserInfoCard } from '@/features/settings/components/UserInfoCard';
 import FullPageLoader from '@/shared/components/ui/loading/FullPageLoader';
+import BackButton from '@/shared/components/ui/BackButton';
 
 export default function SettingsPage() {
   const { data: myInfo, isLoading } = useMyInfoQuery();
@@ -14,19 +15,26 @@ export default function SettingsPage() {
   if (isLoading || !myInfo) return <FullPageLoader text="정보 불러오는 중.." />;
 
   return (
-    <div className="flex flex-col px-2 sm:px-10 space-y-4 overflow-y-auto">
-      {/* 내 정보 */}
-      <UserInfoCard member={myInfo} />
-      <AvatarsDrawer showEditButton={false} showConfirmButton={true} />
-      <Separator />
-      {/* 알림변경 */}
-      <AlarmSettingCard />
-      <Separator />
-      {/* 회원탈퇴 */}
-      <DeleteAccountCard />
-      <Separator />
-      {/* 라이선스 명시 */}
-      <LicenseCard />
+    <div>
+      <div className="p-5 flex items-center gap-2">
+        <BackButton className="shrink-0" />
+        <h1 className="title1-bold leading-none">알림 설정</h1>
+      </div>
+
+      <div className="flex flex-col px-2 sm:px-10 space-y-4 overflow-y-auto">
+        {/* 내 정보 */}
+        <UserInfoCard member={myInfo} />
+        <AvatarsDrawer showEditButton={false} showConfirmButton={true} />
+        <Separator />
+        {/* 알림변경 */}
+        <AlarmSettingCard />
+        <Separator />
+        {/* 회원탈퇴 */}
+        <DeleteAccountCard />
+        <Separator />
+        {/* 라이선스 명시 */}
+        <LicenseCard />
+      </div>
     </div>
   );
 }
