7c86188cacd08fb170a80f1d04855922e4d6ac16
hyemin <hyemin6647@naver.com>
2026-02-12 13:58:39 +0900

    ♻️(#403) UserInfoCard 디자인 수정 및 반응형 개선

--- src/features/settings/components/SettingsSectionCard.tsx
+++ src/features/settings/components/SettingsSectionCard.tsx
@@ -9,7 +9,7 @@ interface SettingsSectionCardProps {
 
 export const SettingsSectionCard = ({ title, desc, children }: SettingsSectionCardProps) => {
   return (
-    <Card className={'shadow-none border-none mb-0'}>
+    <Card className={'shadow-none border-none mb-0 py-5 sm:py-6'}>
       <CardHeader>
         <CardTitle className="title2-bold">{title}</CardTitle>
         {desc && <p className="body2-regular text-gray-500 mt-1 leading-relaxed">{desc}</p>}
