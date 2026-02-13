8bf5f8864eefea9fd484ceea55d35fb8e871cd29
hyemin <hyemin6647@naver.com>
2026-02-13 19:58:03 +0900

    💄(#403) 설정 페이지 헤더 디자인 수정

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -16,11 +16,13 @@ export default function SettingsPage() {
 
   return (
     <div>
-      <div className="p-5 flex items-center gap-2">
-        <BackButton className="shrink-0" />
-        <h1 className="title1-bold leading-none">알림 설정</h1>
-      </div>
+      <nav className="flex justify-between items-center w-full bg-gray-100 border-b border-gray-300 h-14 px-4">
+        <div className="subtitle2-bold sm:title1-bold flex items-center gap-3">
+          <BackButton />
 
+          <p>설정</p>
+        </div>
+      </nav>
       <div className="flex flex-col px-2 sm:px-10 space-y-4 overflow-y-auto">
         {/* 내 정보 */}
         <UserInfoCard member={myInfo} />
