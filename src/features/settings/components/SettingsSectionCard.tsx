451a4fef621f9cb08d031bc9c06afffd9821907c
hyemin <hyemin6647@naver.com>
2026-02-13 20:16:41 +0900

    ♻️(#403) 설정 카드 여백 반응형 조정

--- src/features/settings/components/SettingsSectionCard.tsx
+++ src/features/settings/components/SettingsSectionCard.tsx
@@ -13,7 +13,7 @@ export const SettingsSectionCard = ({ title, desc, children }: SettingsSectionCa
       <CardHeader>
         <CardTitle className="title2-bold">{title}</CardTitle>
         {desc && (
-          <p className="label2-regular sm:body2-regular text-gray-500 mt-3 leading-relaxed">
+          <p className="label2-regular sm:body2-regular text-gray-500 mt-2 sm:mt-3 leading-relaxed">
             {desc}
           </p>
         )}
