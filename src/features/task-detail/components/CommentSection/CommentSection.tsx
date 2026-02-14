3fb6717288517f765559d27ef8c3ecef75a7c8bf
hyemin <hyemin6647@naver.com>
2026-02-14 13:53:11 +0900

    ♻️(#394) 모바일 반응형 개선

--- src/features/task-detail/components/CommentSection/CommentSection.tsx
+++ src/features/task-detail/components/CommentSection/CommentSection.tsx
@@ -17,7 +17,7 @@ const CommentSection = ({ projectId, taskId, comments }: CommentSectionProps) =>
 
   return (
     <div className="bg-gray-100 border-none flex flex-col h-full relative">
-      <h2 className="px-3 py-2.5 sm:px-4 sm:py-3 title2-bold border-b border-gray-300 text-gray-800">
+      <h2 className="px-3 py-2.5 sm:px-4 sm:py-3 label1-bold sm:title2-bold border-b border-gray-300 text-gray-800">
         댓글 ({comments.length})
       </h2>
       <CommentList comments={comments} onSelectPin={handlePinClick} onDelete={handleDelete} />
