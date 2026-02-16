e7c815f99061a67fe6c0b550500b72d5e867eec4
hyemin <hyemin6647@naver.com>
2026-02-16 15:47:26 +0900

    ♻️ (#403) className 문자열 리터럴로 통일

--- src/features/settings/components/SettingsSectionCard.tsx
+++ src/features/settings/components/SettingsSectionCard.tsx
@@ -9,7 +9,7 @@ interface SettingsSectionCardProps {
 
 export const SettingsSectionCard = ({ title, desc, children }: SettingsSectionCardProps) => {
   return (
-    <Card className={'shadow-none border-none mb-0 py-5 sm:py-6'}>
+    <Card className="shadow-none border-none mb-0 py-5 sm:py-6">
       <CardHeader>
         <CardTitle className="title2-bold">{title}</CardTitle>
         {desc && (
