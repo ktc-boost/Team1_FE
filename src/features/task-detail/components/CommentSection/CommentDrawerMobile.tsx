119512dfb5dc1f166a720ba8a095b3817e5035a5
hyemin <hyemin6647@naver.com>
2026-02-16 15:36:11 +0900

    🔥(#394) 불필요한 반응형 삭제

--- src/features/task-detail/components/CommentSection/CommentDrawerMobile.tsx
+++ src/features/task-detail/components/CommentSection/CommentDrawerMobile.tsx
@@ -19,7 +19,7 @@ const CommentDrawerMobile = ({
   return (
     <Drawer open={isCommentOpen} onOpenChange={setIsCommentOpen}>
       <DrawerContent className="h-[90vh]">
-        <div className="flex-1 overflow-y-auto px-1 sm:pb-4">
+        <div className="flex-1 overflow-y-auto px-1">
           <CommentSection projectId={projectId!} taskId={taskId!} comments={comments} />
         </div>
       </DrawerContent>
