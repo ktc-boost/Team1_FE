c53c0d1f7f70d92da01e9e1fcb351be743028c77
hyemin <hyemin6647@naver.com>
2026-02-21 14:55:26 +0900

    ♻️(#412) 파일 페이지 페이지네이션 적용

--- src/features/file/components/FileTableInfo.tsx
+++ src/features/file/components/FileTableInfo.tsx
@@ -1,16 +1,32 @@
 import { useProjectFileSummaryQuery } from '@/features/file/hooks/useProjectFileSummaryQuery';
 import { getTotalFileSize } from '@/features/file/utils/fileUtils';
+import PaginationNav from '@/shared/components/ui/PaginationNav';
 import { useParams } from 'react-router-dom';
 
-const FileTableInfo = () => {
-  const { projectId } = useParams<{ projectId: string }>();
+interface FileTableInfoProps {
+  currentPage: number;
+  setCurrentPage: (page: number | ((prev: number) => number)) => void;
+  pageCount: number;
+}
 
+const FileTableInfo = ({ currentPage, setCurrentPage, pageCount }: FileTableInfoProps) => {
+  const { projectId } = useParams<{ projectId: string }>();
   const { data: fileSummaryData } = useProjectFileSummaryQuery(projectId!);
+  const totalSize = getTotalFileSize([{ sizeBytes: fileSummaryData?.totalSizeBytes ?? 0 }]);
 
   return (
-    <div className="flex items-center justify-between label2-regular sm:label1-regular  text-gray-500 px-2 py-3">
-      <p>총 {fileSummaryData?.totalCount}개 파일</p>
-      <p> 전체 용량 : {getTotalFileSize([{ sizeBytes: fileSummaryData?.totalSizeBytes ?? 0 }])}</p>
+    <div className="flex items-center justify-between label2-regular sm:label1-regular text-gray-500 px-2">
+      <div className="flex items-center gap-4">
+        <p>총 {fileSummaryData?.totalCount ?? 0}개 파일</p>
+        <p className="hidden sm:block text-gray-300">|</p>
+        <p className="hidden sm:block">전체 용량: {totalSize}</p>
+      </div>
+
+      <PaginationNav
+        currentPage={currentPage}
+        pageCount={pageCount}
+        setCurrentPage={setCurrentPage}
+      />
     </div>
   );
 };
