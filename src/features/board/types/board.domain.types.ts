187eb60d68342de909170c41b674b8b2e651e45f
daaoooy <y020d226y@daum.net>
2026-02-24 03:50:21 +0900

    ♻️ (#417) Board 타입 구조 개선

---
+++ src/features/board/types/board.domain.types.ts
@@ -0,0 +1,15 @@
+import type { BOARD_KEYS, BOARD } from '@/features/board/constants/board.domain.constants';
+import type { TaskStatus } from '@/features/task/types/task.domain.types';
+import type { TaskQuery } from '@/features/task/types/task.query.types';
+
+// 컬럼 데이터 타입
+export type ColumnData = {
+  status: TaskStatus;
+  query: TaskQuery;
+};
+
+// 보드 키 타입
+export type BoardKey = (typeof BOARD_KEYS)[keyof typeof BOARD_KEYS];
+
+// 보드 타입
+export type Board = (typeof BOARD)[keyof typeof BOARD];
