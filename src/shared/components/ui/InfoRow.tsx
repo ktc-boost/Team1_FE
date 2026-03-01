890eb86ae89eea7abd8aa9f5dcfe63011bc09dde
daaoooy <y020d226y@daum.net>
2026-03-02 02:55:19 +0900

    💄 (#405) 프로젝트 관리/정보 모달 높이 감소를 위해 내부 콘텐츠 간격 조절

--- src/shared/components/ui/InfoRow.tsx
+++ src/shared/components/ui/InfoRow.tsx
@@ -4,7 +4,7 @@ interface InfoRowProps {
 }
 
 const InfoRow = ({ label, value }: InfoRowProps) => (
-  <div className="flex flex-col gap-1 border-b border-gray-300 pb-2">
+  <div className="flex flex-col gap-1 border-b border-gray-300 py-2">
     <span className="body2-bold md:subtitle1-bold">{label}</span>
     <div className="h-10 flex items-center gap-2 subtitle2-regular pl-1">
       <span className="text-gray-800">{value}</span>
