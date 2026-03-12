67ff4d176a37a0ba231cda320b68a68e17f5650b
hyemin <hyemin6647@naver.com>
2026-03-13 08:54:15 +0900

    💄 (#437) 가독성을 위해 줄바꿈 추가

--- src/shared/components/shadcn/button.tsx
+++ src/shared/components/shadcn/button.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/shared/lib/utils';
+
 const buttonVariantClasses = {
   default: 'bg-primary text-primary-foreground shadow-xs hover:bg-primary/90',
   defaultBoost: 'bg-boost-blue hover:bg-boost-blue-hover text-white shadow-xs',
