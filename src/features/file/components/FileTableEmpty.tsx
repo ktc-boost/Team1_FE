f366d463ca0563dff99f50e02057af7d9b97f80d
daaoooy <y020d226y@daum.net>
2026-03-17 14:47:03 +0900

    💄 (#458) 파일 목록, 메모 목록 통일감 작업
    - 파일이 없을 때와 메모가 없을 때 화면 통일
    - 모바일 파일 목록 페이지처럼 메모 목록 페이지도 열 간의 gab 조절

--- src/features/file/components/FileTableEmpty.tsx
+++ src/features/file/components/FileTableEmpty.tsx
@@ -3,8 +3,8 @@ import { TableRow, TableCell } from '@/shared/components/shadcn/table';
 const FileTableEmpty = () => {
   return (
     <TableRow>
-      <TableCell colSpan={6} className="py-16 text-center text-gray-400">
-        <div className="flex flex-col items-center justify-center">
+      <TableCell colSpan={6}>
+        <div className="flex flex-col items-center justify-center text-gray-400 h-64">
           <div className="text-4xl mb-2">📄</div>
           <div className="body1-regular">파일이 존재하지 않아요!</div>
         </div>
