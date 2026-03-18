9a02e595178117cd171991fd6bd4a267af14a728
hyemin <hyemin6647@naver.com>
2026-03-18 15:12:59 +0900

    ♻️ self-closing 태그 사용

--- src/features/task-detail/components/TaskDetailTopTab/PinCommentTooltip.tsx
+++ src/features/task-detail/components/TaskDetailTopTab/PinCommentTooltip.tsx
@@ -22,8 +22,8 @@ const PinCommentTooltip = ({ hintOpen, onOpenComments, currentPin }: PinCommentT
             <MessageSquare className="size-5 transition-colors text-black" />
             {currentPin && (
               <span className="absolute top-2 right-2 flex h-2 w-2">
-                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-gray-900 opacity-75"></span>
-                <span className="relative inline-flex rounded-full h-2 w-2 bg-gray-900"></span>
+                <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-gray-900 opacity-75" />
+                <span className="relative inline-flex rounded-full h-2 w-2 bg-gray-900" />
               </span>
             )}
           </Button>
