9dc5bf2608b80104db816fa1d8711f15716e8278
daaoooy <y020d226y@daum.net>
2026-03-25 02:17:45 +0900

    ♻️ (#477) 랜딩 페이지 푸터에 카카오톡 문의 추가 + 코드 리팩터링
    - external links 상수 적용
    - a 태그가 아닌 버튼과 핸들러 함수로 변경

--- src/features/landing/components/LandingFooter.tsx
+++ src/features/landing/components/LandingFooter.tsx
@@ -1,35 +1,49 @@
 import { Button } from '@/shared/components/shadcn/button';
 import { Separator } from '@/shared/components/shadcn/separator';
+import { EXTERNAL_LINKS } from '@/shared/constants/external-links';
 import { Github } from 'lucide-react';
 
 const LandingFooter = () => {
+  const handleKakaoInquireClick = () => window.open(EXTERNAL_LINKS.KAKAO_CHAT, '_blank');
+  const handleNaverformInquireClick = () => window.open(EXTERNAL_LINKS.NAVER_FORM, '_blank');
+  const handleGithubClick = () => window.open(EXTERNAL_LINKS.GITHUB, '_blank');
+
   return (
-    <footer className="mt-24 w-screen bg-gradient-to-b from-gray-900 to-black text-white pt-24 pb-12">
+    <footer className="mt-24 w-full bg-gradient-to-b from-gray-900 to-black text-white pt-24 pb-12">
       <div className="mx-auto px-6">
         <div className="flex flex-col items-center text-center">
           <h2 className="text-3xl font-bold mb-6">궁금한 점이 있으신가요?</h2>
 
           <p className="text-gray-300 mb-12 text-sm sm:text-lg leading-relaxed">
             BOOST에 대해 더 자세히 알고 싶거나 도움이 필요하시다면 <br />
-            아래 버튼을 눌러 네이버폼으로 이동해주세요.
+            아래 버튼을 눌러 문의 및 피드백을 남겨주세요.
           </p>
 
           <div className="flex flex-col gap-7 items-center">
-            <Button asChild className="w-60 h-13 subtitle1-regular transition-all">
-              <a href="https://naver.me/G9pDvPJh" target="_blank" rel="noopener noreferrer">
-                네이버폼 바로가기
-              </a>
-            </Button>
+            <div className="flex gap-2">
+              <Button
+                onClick={handleKakaoInquireClick}
+                className="w-40 sm:w-60 h-13 subtitle1-regular rounded-full"
+              >
+                카카오톡 문의
+              </Button>
 
-            <a
-              href="https://github.com/ktc-boost"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center gap-2 text-gray-300 hover:text-white transition-colors"
+              <Button
+                onClick={handleNaverformInquireClick}
+                className="w-40 sm:w-60 h-13 subtitle1-regular rounded-full"
+              >
+                네이버폼 문의
+              </Button>
+            </div>
+
+            <Button
+              variant="ghost"
+              onClick={handleGithubClick}
+              className="gap-2 text-gray-400 hover:bg-transparent hover:text-white"
             >
               <Github className="w-5 h-5" />
               <span className="body2-regular">GitHub에서 프로젝트 보기</span>
-            </a>
+            </Button>
           </div>
 
           <Separator className="mt-16 bg-gray-900/70" />
