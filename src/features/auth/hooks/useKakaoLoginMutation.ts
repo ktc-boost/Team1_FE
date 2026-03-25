890fab724321a8daffb6f614b574efdd80ff3e44
hyemin <hyemin6647@naver.com>
2026-03-25 18:22:32 +0900

    ✨ (#481) 로그인, 회원가입 GA 이벤트 추가

--- src/features/auth/hooks/useKakaoLoginMutation.ts
+++ src/features/auth/hooks/useKakaoLoginMutation.ts
@@ -34,7 +34,11 @@ export const useKakaoLoginMutation = () => {
     },
     onSuccess: (isNewUser) => {
       toast.success('로그인이 완료되었습니다.');
-
+      window.gtag('event', 'login');
+      if (isNewUser) {
+        window.gtag('event', 'sign_up');
+        navigate(ROUTE_PATH.AVATAR);
+      } else if (from) navigate(from, { replace: true });
       if (isNewUser) navigate(ROUTE_PATH.AVATAR);
       else if (from) navigate(from, { replace: true });
       else navigate(ROUTE_PATH.MY_TASK);
