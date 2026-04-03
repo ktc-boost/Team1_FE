c6bfc025c834d4fc718a27f4cc69eb28fd2e7c0b
daaoooy <y020d226y@daum.net>
2026-04-03 13:29:01 +0900

    📱 (#507) 파일 목록 페이지 반응형 개선
    - 태블릿 반응형 추가
    - padding 및 각 cell 너비 조절

--- src/features/file/components/FileTableHeader.tsx
+++ src/features/file/components/FileTableHeader.tsx
@@ -2,23 +2,22 @@ import { TableHeader, TableRow, TableHead } from '@/shared/components/shadcn/tab
 import { cn } from '@/shared/lib/utils';
 
 const FileTableHeader = () => {
-  const mobileHiddenClass = 'hidden md:table-cell';
+  const mobileHiddenClass = 'hidden sm:table-cell';
+  const tabletHiddenClass = 'hidden lg:table-cell';
   const headClass = 'subtitle2-bold text-gray-800';
 
   return (
     <TableHeader className="sticky top-0 z-10 bg-white">
       <TableRow className="border-b border-gray-300 h-12 hover:bg-white">
-        <TableHead className="w-[50px] pl-6 subtitle2-bold text-gray-800 text-center">
-          번호
-        </TableHead>
-        <TableHead className={cn('min-w-[150px] pl-4 text-left', headClass)}>파일명</TableHead>
-        <TableHead className={cn('w-[100px] text-left', headClass, mobileHiddenClass)}>
+        <TableHead className="w-[80px] subtitle2-bold text-gray-800 text-center">번호</TableHead>
+        <TableHead className={cn('min-w-[150px] text-left', headClass)}>파일명</TableHead>
+        <TableHead className={cn('w-[120px] text-left', headClass, mobileHiddenClass)}>
           용량
         </TableHead>
-        <TableHead className={cn('w-[180px] text-left', headClass, mobileHiddenClass)}>
+        <TableHead className={cn('w-[200px] text-left', headClass, tabletHiddenClass)}>
           업로드일
         </TableHead>
-        <TableHead className={cn('w-[200px] text-left', headClass, mobileHiddenClass)}>
+        <TableHead className={cn('w-[120px] lg:w-[200px] text-left', headClass, mobileHiddenClass)}>
           연결된 할 일
         </TableHead>
         <TableHead className={cn('w-[100px] text-center', headClass, mobileHiddenClass)}>
