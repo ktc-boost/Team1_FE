12cff3f825813a97bf6472e47fbc3c2566548cff
daaoooy <y020d226y@daum.net>
2026-04-03 13:32:00 +0900

    ♻️ (#507) 관심사 분리를 위해 mutation 내부에서 처리하던 UI 로직 제거

--- src/features/file/hooks/useFileDownloadMutation.ts
+++ src/features/file/hooks/useFileDownloadMutation.ts
@@ -1,20 +1,12 @@
 import { useMutation } from '@tanstack/react-query';
 import { fetchFileDownloadUrl } from '@/features/file/api/fileDownloadApi';
 import { downloadFromS3 } from '@/features/file/utils/fileDownloadUtil';
-import toast from 'react-hot-toast';
 
 export const useFileDownloadMutation = () => {
   return useMutation({
     mutationFn: async ({ fileId, fileName }: { fileId: string; fileName: string }) => {
       const presigned = await fetchFileDownloadUrl(fileId);
-      //다운로드
       await downloadFromS3(presigned.url, presigned.headers, fileName);
-    },
-    onSuccess: () => {
-      toast.success('파일이 다운로드 되었습니다.');
-    },
-    onError: () => {
-      toast.error('파일 다운로드에 실패했습니다.');
     },
   });
 };
