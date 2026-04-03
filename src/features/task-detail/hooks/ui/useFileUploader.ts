2b28b51505dcba2dd0bb4da38e4b87d5cea6d6a2
daaoooy <y020d226y@daum.net>
2026-04-03 13:25:45 +0900

    ♻️ (#507)  파일 업로드 에러 처리 개선을 위한 콜백 추가

--- src/features/task-detail/hooks/ui/useFileUploader.ts
+++ src/features/task-detail/hooks/ui/useFileUploader.ts
@@ -1,20 +1,25 @@
 import { useDropzone } from 'react-dropzone';
+import toast from 'react-hot-toast';
+import { ApiError } from '@/shared/error/types/apiError.types';
+import { getErrorMessage } from '@/shared/error/utils/error.utils';
 import { useUploadFileMutation } from '@/features/task-detail/hooks/mutation/useFileUploadUrlMutation';
-import { fileToast } from '@/features/task-detail/utils/toast/fileToast';
 
 export const useFileUploader = (taskId: string) => {
   const { mutate: uploadFile } = useUploadFileMutation();
+
   const onDrop = (acceptedFiles: File[]) => {
     acceptedFiles.forEach((file) => {
       uploadFile(
         { file, taskId },
         {
-          onError: (e) => {
-            fileToast.uploadError(e);
+          onError: (error) => {
+            if (error instanceof ApiError) toast.error(getErrorMessage(error));
+            else toast.error('파일 업로드를 실패했어요.');
           },
         },
       );
     });
   };
+
   return useDropzone({ onDrop });
 };
